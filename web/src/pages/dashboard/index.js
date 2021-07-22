db0713c794d7e6f2b3483be6563909a681a48b10
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2021-07-21 23:03:28 -0300

    Start node project and install express

---
+++ web/src/pages/dashboard/index.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from "react";
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Hello Dashboard</h1>
+    </div>
+  );
+}
+
+export default Dashboard;
--- web/src/routes.js
+++ web/src/routes.js
@@ -3,12 +3,14 @@ import { Route, Redirect, BrowserRouter, Switch } from "react-router-dom";
 
 // Pages
 import Home from "./pages/home";
+import Dashboard from "./pages/dashboard";
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/dashboard" exact component={Dashboard} />
 
         {/* <PrivateRoute path="/dashboard" exact component={Dasboard}></PrivateRoute> */}
         {/* 
