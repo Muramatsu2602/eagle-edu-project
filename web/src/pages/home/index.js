50d6b1c317541460a449ebb8fc4594c151b5b963
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2021-07-26 00:08:41 -0300

    Start Homepage

--- web/src/pages/home/index.js
+++ web/src/pages/home/index.js
@@ -1,9 +1,29 @@
 import React, {useState, useEffect} from "react";
+import styled from "styled-components";
+
+import landingPageEagle from '../../assets/eagle_landing_page.png'; // Tell Webpack this JS file uses this image
+
+const DashboardWrapper = styled.div`
+  background-color: var(--white);
+  border-radius: 10px;
+  min-height: 40vh;
+  height: auto;
+  margin: 10rem;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
 
 function Home() {
   return (
     <div>
       <h1>Hello Boss</h1>
+      <DashboardWrapper>
+        <img src={landingPageEagle} alt="eagle from landing"/>
+      </DashboardWrapper>
     </div>
   );
 }
