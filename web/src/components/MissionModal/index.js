74e17b41bd5ee4ee33b539f6079878ceb831a323
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2021-08-02 00:46:42 -0300

    Improving Modal visuals

--- web/src/components/MissionModal/index.js
+++ web/src/components/MissionModal/index.js
@@ -1,6 +1,34 @@
 import React from "react";
-import styled from "styled-components";
+
+import {
+  ModalContent,
+  Background,
+  ModalWrapper,
+  CloseModalButton,
+} from "./mission-modal-styling";
 
 export const MissionModal = ({ showModal, setShowModal }) => {
-  return <>{showModal ? <div> Modal </div> : null}</>;
+  const completeTask = () => {
+    alert("task completed successfully!");
+  };
+
+  return (
+    <>
+      {showModal ? (
+        <Background>
+          <ModalWrapper showModal={showModal}>
+            <ModalContent>
+              <h1>Current Mission</h1>
+              <p>Press the button to complete the mission</p>
+              <button onClick={completeTask}>Click me!</button>
+            </ModalContent>
+            <CloseModalButton
+              aria-label="Close modal"
+              onClick={() => setShowModal((prev) => !prev)}
+            />
+          </ModalWrapper>
+        </Background>
+      ) : null}
+    </>
+  );
 };
